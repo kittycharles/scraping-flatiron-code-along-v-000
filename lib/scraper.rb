2ad87116c7cea5b5759d6e0eed19a20ff53444a3
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-06 17:53:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,21 +16,23 @@ class Scraper
       # end
     end
 
-
-
-
-
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+    def get_courses
+      self.get_page.css(".post")
     end
-  end
 
-end
+
+
+        def print_courses
+        self.make_courses
+        Course.all.each do |course|
+          if course.title
+            puts "Title: #{course.title}"
+            puts "  Schedule: #{course.schedule}"
+            puts "  Description: #{course.description}"
+          end
+        end
+      end
+
+    end
 
 Scraper.new.get_page
